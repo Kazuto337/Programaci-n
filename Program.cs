0bd5a6fb135eba0481a45f61a03a03c03b85d1ce
SantiagoLara902 <60271850+SantiagoLara902@users.noreply.github.com>
2020-01-24 14:08:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Programa_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.WriteLine("Ingrese el valor de C");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de Z");
+            double Z = double.Parse(Console.ReadLine());
+
+            //Conversion de Radianes a grados
+            // Coversion de grados a radianes
+            double cGrados = (Math.PI / 180) * c;
+
+            //hallar la variable Y
+            double Y = Z / ( Math.Tan(cGrados));
+            
+            //hallar la variable a
+            double a = 90 - c;
+
+            //hallar la variable t
+            double t = Math.Sqrt((Y * Y) + (Z * Z));
+
+            Console.WriteLine("Sus resultados son:" + " " + "a=" + a + " " + "Y=" + Y + " " + " t=" + t );
+           
+        }
+    }
+}
