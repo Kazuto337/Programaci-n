0989dfe877a88dc2fafb1d2bb04ea07f55aa83e8
SantiagoLara902 <60271850+SantiagoLara902@users.noreply.github.com>
2020-01-27 10:02:32 -0500

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Programa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos Conocidos
+            Console.WriteLine("Ingrese el valor de Z");
+            double Z = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de Y");
+            double Y = double.Parse(Console.ReadLine());
+
+            //Solución para hallar "t"
+            double t = Math.Sqrt((Z * Z) + (Y * Y));
+
+
+            //Solución para hallar "c" 
+            double c = Math.Atan(Z / Y) * (180/Math.PI);
+
+            //Solución para hallar "c"
+            double a = (90 - c);
+
+            //Resultados
+            Console.WriteLine("Sus Resultados son" + " " + "t =" + t + " " + "c = " + c + " " + "a=" + a);
+            
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Programa_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de t");
+            double t = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el valor de a");
+            double a = double.Parse(Console.ReadLine());
+
+            //Hallar el valor de "c"
+            double c = 90 - a;
+
+            // Coversion de grados a radianes
+            double cGrados = (Math.PI / 180) * c; 
+
+            //Hallar el valor de Y
+            double Y = t * Math.Cos(cGrados);
+
+            //Hallar el valor Z
+            double Z = Math.Sqrt((t * t) - (Y * Y));
+
+            //Solución
+            Console.WriteLine("Tus resultados son:" + " " + "c =" + c + " " + " Z =" + Z + " " + "Y =" + Y);
+
+        }
+    }
+}
