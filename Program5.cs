f8ccbe220d49e1b8391039c6b25babec610c3d0f
SantiagoLara902 <60271850+SantiagoLara902@users.noreply.github.com>
2020-01-31 07:22:28 -0500

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor de su sueldo para asignarle la tarifa correspondiente");
+            double s = double.Parse(Console.ReadLine());
+
+            if (s <= 877803)
+            {
+                Console.WriteLine("La tarifa designada fue la Tarifa A");
+            }
+            else if(s >= (877803 * 2) && s<= (877803 * 4))
+            {
+                Console.WriteLine("La tarifa designada fue la Tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("La Tarifa designada fue la Tarifa C");
+            }
+        }
+    }
+}
---
+++ Program6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Programa_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso del sueldo
+            Console.WriteLine("Ingrese el valor de su sueldo para asignarle su taifa");
+            double s = double.Parse(Console.ReadLine());
+
+            //asignacion de la tarifa
+            if (s <= (877803 * 2))
+            {
+                Console.WriteLine("Su tarifa a pagar sera la Tarifa A");
+
+            }
+            else if ( s>= (877803 * 2) && s <= (877803 * 5))
+            {
+                Console.WriteLine("Su tarifa a pagar sera la Tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Su tarifa a pagar sera la Tarifa C");
+            }
+
+        }
+    }
+}
