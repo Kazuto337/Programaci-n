01a831b4846c6fafd564bc2d115c8c9ae0b7651c
SantiagoLara902 <60271850+SantiagoLara902@users.noreply.github.com>
2020-02-17 14:44:15 -0500

    Update Program9.cs

--- Program9.cs
+++ Program9.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Programa9
 {
@@ -8,7 +8,7 @@ namespace Programa9
         {
             Random aleatorio = new Random();
             int Dado1 = 0, Total = 0, Turnos = 0;
-            bool PrimeraV = true, Continuar = true;
+            bool PrimeraV = true, Continuar = true , Condicion1012 = false;
 
             while (Total < 100 && Continuar == true)
             {
@@ -27,22 +27,32 @@ namespace Programa9
                     Dado1 = aleatorio.Next(1, 13);
                     Total = Total + Dado1;
                     Console.WriteLine("Tu nuevo dado fue de " + Dado1 + " Y tu puntuacion actual es de " + Total);
-                   
-                    if (Dado1 == 10 && Turnos > 3)
+
+                    if (Dado1 == 10)
                     {
-                        if (Dado1 == 12)
-                        {
-                            Console.WriteLine("Ganaste :D sacaste un 12 despues de un 10 ");
-                        }
-                        else
-                        {
-                            return;
-                        }
+                        Condicion1012 = true;
+                       
+                    }
+                    else
+                    {
+                        Condicion1012 = false;
+                    }
+                    if (Dado1 == 12 && Condicion1012)
+                    {
+                        Console.WriteLine("Ganaste :D sacaste un 12 despues de un 10 ");
+                        break;
+                    }
+                    
+                    if (Dado1 %2 != 0 && Turnos > 3)
+                    {
+                        Console.WriteLine("Tu dado fue de " + Dado1 +" Perdiste por ser impar");
+                        break;
                     }
                 }
-                if(Total >= 100)
+                if (Total >= 100)
                 {
                     Console.WriteLine("Felicidades Ganaste, tu puntuacion final fue de " + Total);
+                    break;
                 }
                 Console.WriteLine("Desea Continuar S/N");
                 if (Console.ReadLine().Equals("s"))
